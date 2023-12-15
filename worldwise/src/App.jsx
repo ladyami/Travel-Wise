e67adc024f7e5ab50bd17fb1fa3adeb95cc450fd
Amina Oyegoke <99187115+ladyami@users.noreply.github.com>
2023-12-15 20:33:17 +0100

    Create app routing

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,14 +1,24 @@
 //import { useState } from 'react'
+import {BrowserRouter, Route, Routes } from "react-router-dom";
 
 import './App.css'
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   
 
   return (
-    <div>
-      hello world
-     </div>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Homepage/>} />
+      <Route path="product" element={<Product/>} />
+      <Route path="pricing" element={<Pricing/>} />
+      <Route path="*" element={<PageNotFound/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
