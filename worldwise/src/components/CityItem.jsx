608f05307c6ea239528ea5946474cbb7bf84f938
Amina Oyegoke <99187115+ladyami@users.noreply.github.com>
2023-12-19 23:43:12 +0100

    Finish Implementation

--- worldwise/src/components/CityItem.jsx
+++ worldwise/src/components/CityItem.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import 
+import styles from './CityItem.module.css';
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    
+  }).format(new Date(date));
+
 
 
 const CityItem = ({city}) => {
+const{cityName, emoji, date} = city;
+
   return (
-    <li>city</li>
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.nmae}> {cityName} </h3>
+      <time className={styles.date}>{formatDate(date)}</time>
+      <button className={styles.deleteBtn}> &times; </button>
+    </li>
   )
 }
 
--- worldwise/src/components/CityList.jsx
+++ worldwise/src/components/CityList.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Sipnner from './Sipnner';
 import styles from './CityList.module.css';
 import CityItem from './CityItem';
+import Message from './Message'
 
 const CityList = ({cities, isLoading}) => {
 
    if(isLoading) return <Sipnner/>;
-
+   if (!cities.length) return (  <Message message='Add your first city by clicking on a city on the map' />
+   );
   return (
-    <ul className={styles.CityList}>
+    <ul className={styles.cityList}>
       {cities.map((city) => (  <CityItem city={city} key={city.id} />))}
     </ul>
   )
