b1aac7e5c36df466c9dc233cae721bd3979b7aa4
Amina Oyegoke <99187115+ladyami@users.noreply.github.com>
2024-01-04 18:38:32 +0100

    add react leaflet

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./Map.module.css";
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
 
 const Map = () => {
 
   const navigate = useNavigate();
+  const [ mapPosition, setMapPosition ] = useState([40, 0])
 
   const [searchParams, setSearchParams] = useSearchParams()
 
@@ -14,14 +16,18 @@ const Map = () => {
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
+    <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+    />
+    <Marker position={mapPosition}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
       
-      <h1>Map</h1>
-      <h1>
-        position:{lat}, {lng}
-      </h1>
-      <button onClick={() =>  {setSearchParams({lat: 23, lng: 50}); } }>
-        Change pos
-      </button>
     </div>
   )
 }
