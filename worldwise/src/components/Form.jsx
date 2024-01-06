2e696e71b08f35bccaf036ac0d857971447bf385
Amina Oyegoke <99187115+ladyami@users.noreply.github.com>
2024-01-06 20:29:35 +0100

    fetch city data in the form

--- worldwise/src/components/Form.jsx
+++ worldwise/src/components/Form.jsx
@@ -1,10 +1,13 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
+import { useUrlPosition } from "../hooks/useUrlPosition";
+import Message from "./Message";
+import Sipnner from './Sipnner';
 
 
 
@@ -16,13 +19,42 @@ export function convertToEmoji(countryCode) {
   return String.fromCodePoint(...codePoints);
 }
 
-function Form() {
+const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
- 
+function Form() {
+  
+  const [lat, lng] = useUrlPosition();
+  const [isLoadingGeocoding,  setIsLoadingGeocoding] =useState(false)
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const [emoji, setEmoji ] = useState("");
+  const [ geoCodingError, setGeocodingError] = useState("")
+  
+  useEffect( function() {
+    async function fetchCityData() {
+      try {
+        setIsLoadingGeocoding(true)
+        setGeocodingError("");
+        const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`)
+        const data = await res.json();
+        
+        if(!data.countryCode) throw new Error("That doesn't seem to be a city. Click somewhere else 😇 " )
+        setCityName(data.city || data.locality || "");
+        setCountry(data.countryName);
+        setEmoji(convertToEmoji(data.countryCode));
+      } catch (error) {
+        setGeocodingError(error.message)
+      } finally{
+        setIsLoadingGeocoding(false)
+      }
+    }
+    fetchCityData();
+  }, [lat, lng] );
+  
+  if(isLoadingGeocoding)  return <Sipnner />
+  if(geoCodingError) return <Message message={geoCodingError} />
 
   return (
     <form className={styles.form}>
@@ -33,7 +65,7 @@ function Form() {
           onChange={(e) => setCityName(e.target.value)}
           value={cityName}
         />
-        {/* <span className={styles.flag}>{emoji}</span> */}
+         <span className={styles.flag}>{emoji}</span> 
       </div>
 
       <div className={styles.row}>
--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import styles from "./Map.module.css";
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate,  } from 'react-router-dom';
 import {MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents} from 'react-leaflet';
 import { useCities } from '../contexts/CitiesContext';
 import { useGeolocation } from '../hooks/useGeolocation';
 import Button from '../components/Button'
+import { useUrlPosition } from '../hooks/useUrlPosition';
 
 
 const Map = () => {
   const navigate = useNavigate();
   const { cities} = useCities();
   const [ mapPosition, setMapPosition ] = useState([40, 0]);
-  const [searchParams] = useSearchParams();
+ // const [searchParams] = useSearchParams();
   const {isLoading: isLoadingPosition, position: geoLocationPosition, getPosition} = useGeolocation();
 
-  const mapLat = searchParams.get('lat');
-  const mapLng = searchParams.get('lng');
+  //const mapLat = searchParams.get('lat');
+  //const mapLng = searchParams.get('lng');
+
+   const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(
   function() { 
